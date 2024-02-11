3700de38868f173977fdf6ef59cbdeb489f6aefa
Shweta Dattukumar <Shweta.dattukumar@iress.com>
2024-02-11 19:05:58 +0000

    Committing the project.

---
+++ TestProject1/ServerConfigReaderTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConfigEditor.Controllers;
+using ConfigEditor.Domain;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class ServerConfigReaderTests
+    {
+        private ServerConfigReader _serverConfigReader;
+
+
+        [TestMethod]
+        public async Task ServerConfigReader_SuccessTest()
+        {
+            _serverConfigReader = new ServerConfigReader();
+
+            var result = _serverConfigReader.GetServerConfig();
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
